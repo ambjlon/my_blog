3ac89012825fcb24f2bebac71e55750ef35a0132
Chen Jianglong <ambjlon@163.com>
2015-07-11 10:49:28 -0400

    mod run.sh in los angeles

--- data/run.sh
+++ data/run.sh
@@ -1,23 +1,24 @@
-sudo python insert_txtblog_2_mysql.py '2013.3.31百度之星，线性同余发生器 求序列周期（1）'  '算法' '百度之星 同余 伪随机数' baiduzhixing.md
-sudo python insert_txtblog_2_mysql.py '2013.3.31百度之星，线性同余发生器 求序列周期（2）---给出一个正解'  '算法' '百度之星 同余 伪随机数' baiduzhixing_solution.md
-sudo python insert_txtblog_2_mysql.py '搭建自由域名的个人博客'  '工程技术' '个人博客 云服务器' buy_server.md 
-sudo python insert_txtblog_2_mysql.py '允许访问文件网址 chrome插件markdown preview plus'  '工程技术' 'chrome插件 markdown'  chrome_markdown_preview_plus.md 
-sudo python insert_txtblog_2_mysql.py '我的emacs配置备忘录'  '工程技术'  'emacs' emacs.md 
-sudo python insert_txtblog_2_mysql.py '安装 django mysql python 简单配置'  '工程技术' '配置 django mysql Python' install_django_mysql_python.md 
-sudo python insert_txtblog_2_mysql.py 'Java正则匹配数字和字母以及空格的组合'  '工程技术' '正则表达式' java_regular_az09_combine.md 
-sudo python insert_txtblog_2_mysql.py 'LDA学习笔记（1）'  '算法' 'LDA 主体模型' LDA1.md 
-sudo python insert_txtblog_2_mysql.py 'LDA学习资料汇总'  '算法' 'LDA 主题模型' LDA_document_summary.md 
-sudo python insert_txtblog_2_mysql.py 'linux下python 交互命令行定制自动补全'  '工程技术' 'linux Python 自动补全' linux_python_intractive_complement.md 
-sudo python insert_txtblog_2_mysql.py 'linux screen的使用'  '工程技术' 'screen shell linux' linux_screen.md 
-sudo python insert_txtblog_2_mysql.py 'Linux shell date 按照时间片段进行循环'  '工程技术' 'linux shell' linux_shell_date.md 
-sudo python insert_txtblog_2_mysql.py 'mac os x ssh to linux 使用 gnuplot'  '工程技术' 'gunplot linux' macox_ssh_linux_gnuplot_conf.md 
-sudo python insert_txtblog_2_mysql.py 'matlab封装DLL动态链接库 再用VS2010调用（matlab VS混合编程）'  '工程技术' 'matlab vs' matlab_dll.md 
-sudo python insert_txtblog_2_mysql.py 'matlab kl-divergence（KL散度）实现代码'  '算法' 'matlab KL距离' matlab_kl_distance.md 
-sudo python insert_txtblog_2_mysql.py '寻找空间点集中与指定点距离最近的点-由一道面试题目想到的'  '算法' '面试题' mianshiti_kd_distance.md 
-sudo python insert_txtblog_2_mysql.py '编程之美--寻找符合条件的整数，符合条件整数存在的证明'  '算法' '编程之美 面试题' program_beauty_integer.md 
-sudo python insert_txtblog_2_mysql.py '编程之美--小飞的电梯调度算法的一点论证'  '算法' '编程之美 电梯调度' program_beauty_xiaofen_dianti.md 
-sudo python insert_txtblog_2_mysql.py '自动扫雷 分析'  '算法' '扫雷' saolei_any.md 
-sudo python insert_txtblog_2_mysql.py '对后缀数组构造算法--DC3的一点解释'  '算法' '面试题 后缀数组'  suffix_dc3.md 
-sudo python insert_txtblog_2_mysql.py '树状数组的原理解释'  '算法' '树状数组 面试题' tree_array.md
-sudo python insert_txtblog_2_mysql.py '我是运维'  '工程技术' 'linux shell' woshi_pe.md 
-sudo python insert_txtblog_2_mysql.py 'Z shell（zsh）安装-更新-配置-使用'  '工程技术' 'Zshell shell linux' zshell.md 
+#sudo python insert_txtblog_2_mysql.py '2013.3.31百度之星，线性同余发生器 求序列周期（1）'  '算法' '百度之星 同余 伪随机数' baiduzhixing.md
+#sudo python insert_txtblog_2_mysql.py '2013.3.31百度之星，线性同余发生器 求序列周期（2）---给出一个正解'  '算法' '百度之星 同余 伪随机数' baiduzhixing_solution.md
+#sudo python insert_txtblog_2_mysql.py '搭建自由域名的个人博客'  '工程技术' '个人博客 云服务器' buy_server.md 
+#sudo python insert_txtblog_2_mysql.py '允许访问文件网址 chrome插件markdown preview plus'  '工程技术' 'chrome插件 markdown'  chrome_markdown_preview_plus.md 
+#sudo python insert_txtblog_2_mysql.py '我的emacs配置备忘录'  '工程技术'  'emacs' emacs.md 
+#sudo python insert_txtblog_2_mysql.py '安装 django mysql python 简单配置'  '工程技术' '配置 django mysql Python' install_django_mysql_python.md 
+#sudo python insert_txtblog_2_mysql.py 'Java正则匹配数字和字母以及空格的组合'  '工程技术' '正则表达式' java_regular_az09_combine.md 
+#sudo python insert_txtblog_2_mysql.py 'LDA学习笔记（1）'  '算法' 'LDA 主体模型' LDA1.md 
+#sudo python insert_txtblog_2_mysql.py 'LDA学习资料汇总'  '算法' 'LDA 主题模型' LDA_document_summary.md 
+#sudo python insert_txtblog_2_mysql.py 'linux下python 交互命令行定制自动补全'  '工程技术' 'linux Python 自动补全' linux_python_intractive_complement.md 
+#sudo python insert_txtblog_2_mysql.py 'linux screen的使用'  '工程技术' 'screen shell linux' linux_screen.md 
+#sudo python insert_txtblog_2_mysql.py 'Linux shell date 按照时间片段进行循环'  '工程技术' 'linux shell' linux_shell_date.md 
+#sudo python insert_txtblog_2_mysql.py 'mac os x ssh to linux 使用 gnuplot'  '工程技术' 'gunplot linux' macox_ssh_linux_gnuplot_conf.md 
+#sudo python insert_txtblog_2_mysql.py 'matlab封装DLL动态链接库 再用VS2010调用（matlab VS混合编程）'  '工程技术' 'matlab vs' matlab_dll.md 
+#sudo python insert_txtblog_2_mysql.py 'matlab kl-divergence（KL散度）实现代码'  '算法' 'matlab KL距离' matlab_kl_distance.md 
+#sudo python insert_txtblog_2_mysql.py '寻找空间点集中与指定点距离最近的点-由一道面试题目想到的'  '算法' '面试题' mianshiti_kd_distance.md 
+#sudo python insert_txtblog_2_mysql.py '编程之美--寻找符合条件的整数，符合条件整数存在的证明'  '算法' '编程之美 面试题' program_beauty_integer.md 
+#sudo python insert_txtblog_2_mysql.py '编程之美--小飞的电梯调度算法的一点论证'  '算法' '编程之美 电梯调度' program_beauty_xiaofen_dianti.md 
+#sudo python insert_txtblog_2_mysql.py '自动扫雷 分析'  '算法' '扫雷' saolei_any.md 
+#sudo python insert_txtblog_2_mysql.py '对后缀数组构造算法--DC3的一点解释'  '算法' '面试题 后缀数组'  suffix_dc3.md 
+#sudo python insert_txtblog_2_mysql.py '树状数组的原理解释'  '算法' '树状数组 面试题' tree_array.md
+#sudo python insert_txtblog_2_mysql.py '我是运维'  '工程技术' 'linux shell' woshi_pe.md 
+#sudo python insert_txtblog_2_mysql.py 'Z shell（zsh）安装-更新-配置-使用'  '工程技术' 'Zshell shell linux' zshell.md 
+sudo python insert_txtblog_2_mysql.py  'mac os x使用emacs' '工程技术' 'emacs homebrew mac'  macosx_using_emacs.md
