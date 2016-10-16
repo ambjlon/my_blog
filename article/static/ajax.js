f5b9364a8e153debac389dc058447a7fce11d73a
ambjlon@163.com <ambjlon@163.com>
2016-10-16 21:48:04 +0800

    changyan

--- article/static/ajax.js
+++ article/static/ajax.js
@@ -134,14 +134,13 @@ $(document).ready(function() {
         });
         
         //多说公共JS代码 start (一个网页只需插入一次)
-        setTimeout(function() {
-            
-            var appid = 'cysE7DNJ7';
-            var conf = 'prod_5f40a54419bd75d503c5e7bd5916dc79';
-            var width = window.innerWidth || document.documentElement.clientWidth;
-            if (width < 960) {
-                window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id="' + appid + '&conf=' + conf + '"><\/script>"'); } else { var loadJs=function(d,a){var c=document.getElementsByTagName("head")[0]||document.head||document.documentElement;var b=document.createElement("script");b.setAttribute("type","text/javascript");b.setAttribute("charset","UTF-8");b.setAttribute("src",d);if(typeof a==="function"){if(window.attachEvent){b.onreadystatechange=function(){var e=b.readyState;if(e==="loaded"||e==="complete"){b.onreadystatechange=null;a()}}}else{b.onload=a}}c.appendChild(b)};loadJs("http://changyan.sohu.com/upload/changyan.js",function(){window.changyan.api.config({appid:appid,conf:conf})}); } ;
-        }, 5000);
+        // setTimeout(function() {            
+        //     var appid = 'cysE7DNJ7';
+        //     var conf = 'prod_5f40a54419bd75d503c5e7bd5916dc79';
+        //     var width = window.innerWidth || document.documentElement.clientWidth;
+        //     if (width < 960) {
+        //         window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id="' + appid + '&conf=' + conf + '"><\/script>"'); } else { var loadJs=function(d,a){var c=document.getElementsByTagName("head")[0]||document.head||document.documentElement;var b=document.createElement("script");b.setAttribute("type","text/javascript");b.setAttribute("charset","UTF-8");b.setAttribute("src",d);if(typeof a==="function"){if(window.attachEvent){b.onreadystatechange=function(){var e=b.readyState;if(e==="loaded"||e==="complete"){b.onreadystatechange=null;a()}}}else{b.onload=a}}c.appendChild(b)};loadJs("http://changyan.sohu.com/upload/changyan.js",function(){window.changyan.api.config({appid:appid,conf:conf})}); } ;
+        // }, 2000);
         //多说公共JS代码 end
 
         //异步加载需要调用js渲染latex公式 结束
